d23176734ec4438ff8af58d269c005a9b37fa273
pritam guha <pritam@tenxerlabs.com>
2024-03-04 10:52:40 +0530

    moved

--- src/Component/Map/Practice3.js
+++ src/Component/Map/Practice3.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import Button from '@mui/material/Button';
-import { Data } from '../UseState/Data';
+import { Data } from './Data';
 
 const Practice3 = () => {
     const [text, setText] = useState(false)
